eaa8872cd1928b3d73a54ad8fb7882f1192e5611
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-04-03 12:12:47 -0500

    clean

--- ingest/04-clean-to-semantic.R
+++ ingest/04-clean-to-semantic.R
@@ -24,17 +24,17 @@ municipios = as_tibble(dbGetQuery(con, "select * from clean.explorador_municipio
 colnames(municipios) = dbSafeNames(colnames(municipios))
 municipios<-round_df(municipios, digits=3)
 
-municipios<-municipios %>% dplyr::select(cve_muni,nom_mun,cve_ent,pobreza_p_10,pobreza_m_p_10, pobreza_e_p_10, vul_car_p_10, vul_ing_p_10, 
-                                         no_pobv_p_10, pobreza_n_10, pobreza_m_n_10, pobreza_e_n_10, vul_car_n_10, vul_ing_n_10, no_pobv_n_10,
-                                         s052, p_0424, s057, s061, s065, s071, s072, s118, s174, s176, s216, s241, u009, e003, p_0196, s017, e016, p_0342,
-                                         gini_90,gini_00, gini_10,irez_soc10,irez_soc00,irez_soc05,irez_soc15,pobtot_15,porc_pob_snservsal15,porc_pob_15_analfa15,
-                                         porc_snaguaent15,porc_vivsndren15,porc_vivsnenergia15,porc_vivsnsan15,carencias_n_10,ic_ali_n_10,
-                                         ic_segsoc_n_10,ic_sbv_n_10,ic_asalud_n_10,ic_cev_n_10,plb_n_10,ic_rezedu_n_10,carencias3_n_10,
-                                         pobreza_n_10,pobreza_e_n_10,pobreza_m_n_10,no_pobv_n_10,vul_car_n_10,vul_ing_n_10,plb_p_10,
-                                         carencias_p_10,ic_ali_p_10,ic_segsoc_p_10,ic_sbv_p_10,ic_asalud_p_10,ic_cev_p_10,ic_rezedu_p_10)
+#municipios<-municipios %>% dplyr::select(cve_muni,nom_mun,cve_ent,pobreza_p_10,pobreza_m_p_10, pobreza_e_p_10, vul_car_p_10, vul_ing_p_10, 
+#                                         no_pobv_p_10, pobreza_n_10, pobreza_m_n_10, pobreza_e_n_10, vul_car_n_10, vul_ing_n_10, no_pobv_n_10,
+#                                         s052, p_0424, s057, s061, s065, s071, s072, s118, s174, s176, s216, s241, u009, e003, p_0196, s017, e016, p_0342,
+                                         # gini_90,gini_00, gini_10,irez_soc10,irez_soc00,irez_soc05,irez_soc15,pobtot_15,porc_pob_snservsal15,porc_pob_15_analfa15,
+                                         # porc_snaguaent15,porc_vivsndren15,porc_vivsnenergia15,porc_vivsnsan15,carencias_n_10,ic_ali_n_10,
+                                         # ic_segsoc_n_10,ic_sbv_n_10,ic_asalud_n_10,ic_cev_n_10,plb_n_10,ic_rezedu_n_10,carencias3_n_10,
+                                         # pobreza_n_10,pobreza_e_n_10,pobreza_m_n_10,no_pobv_n_10,vul_car_n_10,vul_ing_n_10,plb_p_10,
+                                         # carencias_p_10,ic_ali_p_10,ic_segsoc_p_10,ic_sbv_p_10,ic_asalud_p_10,ic_cev_p_10,ic_rezedu_p_10)
 
 
-municipios <- municipios %>% dplyr::mutate_each(funs(as.numeric(.)),matches("^[s|p[_]|e|u][0-9]+", ignore.case=FALSE))
+municipios <- municipios %>% dplyr::mutate_each(funs(as.numeric(.)),matches("^[s|p_|e|u][0-9]+", ignore.case=FALSE))
 dbGetQuery(con, "DROP TABLE semantic.explorador_municipios;")
 dbWriteTable(con, c("semantic",'explorador_municipios'),municipios, row.names=FALSE)
 
@@ -56,14 +56,14 @@ dbWriteTable(con, c("semantic",'explorador_municipios_dic'),municipios_dic, row.
 ##################
 ## Explorador Estados
 ##################
-estados = as_tibble(dbGetQuery(con, "select * from clean.explorador_estados;"))
+estados = as_tibble(dbGetQuery(con, "select * from clean.explorador_estados WHERE cve_ent!='33' AND cve_ent IS NOT NULL;")) 
 colnames(estados) = dbSafeNames(colnames(estados))
 estados<-round_df(estados, digits=3)
 
 #select variables
 #estados<-estados %>% dplyr::select()
 
-estados <- estados %>% dplyr::mutate_each(funs(as.numeric(.)),matches("^[s|p[_]|n[_]|e|u][0-9]+", ignore.case=FALSE))
+estados <- estados %>% dplyr::mutate_each(funs(as.numeric(.)),matches("^[s|p_|n_|e|u][0-9]+", ignore.case=FALSE))
 dbGetQuery(con, "DROP TABLE semantic.explorador_estados;")
 dbWriteTable(con, c("semantic",'explorador_estados'),estados, row.names=FALSE)
 clean_dic <- colnames(estados)
