a87429c9efbdb1174466df91c3f5f327b8bd6626
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-05-16 19:38:24 -0500

    aws config bash version

--- aws_ip_group.sh
+++ aws_ip_group.sh
@@ -1,6 +1,4 @@
-""" 
-Script to add local ip into aws security group
-"""
+#Script to add local ip into aws security group
 
-alias myip='export IP=`curl -s https://api.ipify.org`;echo $IP'  
-aws ec2 authorize-security-group-ingress --protocol tcp --port 5432 --cidr `myip`/32 --group-id  sg-935a01ea  
+myip="$( curl -s https://api.ipify.org )"
+aws ec2 authorize-security-group-ingress --protocol tcp --port 5432 --cidr $myip/32 --group-id  sg-935a01ea  
