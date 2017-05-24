aa398b86e608d45b9549c5c730d1c24426e0965d
javier <javurena7@gmail.com>
2017-05-24 17:36:28 -0500

    Change transparencia pipes

--- politica_preventiva/pipelines/ingest/bash_scripts/transparencia.sh
+++ politica_preventiva/pipelines/ingest/bash_scripts/transparencia.sh
@@ -11,4 +11,6 @@ echo "Descarga Presupuesto Desarrollo Social De 2008 a 2017"
 # Descargar
 
 mkdir -p $1
-wget "https://s3.amazonaws.com/datastore.openspending.org/6018ab87076187018fc29c94a68a3cd2/presupuesto-mexico-2008-20164t-2017/data/presupuesto_mexico_2008_20164t_2017.csv" -O $2
+a="/temp.csv"
+wget "https://s3.amazonaws.com/datastore.openspending.org/6018ab87076187018fc29c94a68a3cd2/presupuesto-mexico-2008-20164t-2017/data/presupuesto_mexico_2008_20164t_2017.csv" -O $1$a
+csvformat -D "|" $1$a > $2
