053361a5d1f6215916bd7133511e1eab57e3a44d
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-05-24 12:49:37 -0500

    setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plataforma_preventiva-pipeline: Pipeline plataforma_preventiva
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.org'), encoding='utf-8') as f:    
+with open(path.join(here, 'README.Rmd'), encoding='utf-8') as f:    
     long_description = f.read()
 
 setup(
