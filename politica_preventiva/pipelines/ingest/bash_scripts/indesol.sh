1ce89516cd61f594001312a12c74c0202db2ba81
Ubuntu <ubuntu@ip-172-31-29-116.us-west-2.compute.internal>
2017-05-29 15:56:03 +0000

    fix indesol

--- politica_preventiva/pipelines/ingest/bash_scripts/indesol.sh
+++ politica_preventiva/pipelines/ingest/bash_scripts/indesol.sh
@@ -6,4 +6,4 @@ wget -O "$1/indesol.csv" "http://166.78.45.36/portal/organizaciones/excel/?cluni
 
 echo 'Clean'
 csvclean -d ','  -e 'latin1' -v $1/indesol.csv
-#sed  "s/,,/,NA,/g; s/^,/NA,/g; s/,$/,NA/g; s/^N\/A,,/NA,/g; s/,N\/A,/,NA,/g; s/,N\/A$/,NA/g; s/|/-/g; s/,/|/g;" $1/indesol_out.csv > $2
+sed  "s/,,/,NA,/g; s/^,/NA,/g; s/,$/,NA/g; s/^N\/A,,/NA,/g; s/,N\/A,/,NA,/g; s/,N\/A$/,NA/g; s/|/-/g; s/,/|/g;" $1/indesol_out.csv > $2
