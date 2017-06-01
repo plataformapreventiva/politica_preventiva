b1b46f59e3510758b20d8c1e5532d11f0a1dc0dd
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-06-01 18:03:48 -0500

    s3 to pandas fun

--- politica_preventiva/pipelines/utils/pipeline_utils.py
+++ politica_preventiva/pipelines/utils/pipeline_utils.py
@@ -21,7 +21,10 @@ from itertools import product
 import boto3
 
 def s3_to_pandas(Bucket,Key,sep="|"):
-
+    """
+    Downloads csv from s3 bucket into a pandas Dataframe
+    Assumes aws keys as environment variables
+    """
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket=Bucket,Key=Key)
 
