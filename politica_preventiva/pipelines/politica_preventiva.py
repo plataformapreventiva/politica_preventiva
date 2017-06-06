8c2f553982eb1a8d4db8c42145cde6761f6c2a20
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-06-06 09:28:07 -0500

    month - date

--- politica_preventiva/pipelines/politica_preventiva.py
+++ politica_preventiva/pipelines/politica_preventiva.py
@@ -17,20 +17,20 @@ from joblib import Parallel, delayed
 from itertools import product
 from dotenv import load_dotenv,find_dotenv
 
-from ingest.ingest_orchestra import UpdateDB, LocalToS3
+from ingest.ingest_orchestra import UpdateOutput, LocalToS3, Concatenation
 from etl.etl_orchestra import ETL
-from utils.pipeline_utils import parse_cfg_list, extra_parameters
+from utils.pipeline_utils import parse_cfg_list, extra_parameters, historical_dates
 import pdb
 
 logger = logging.getLogger("dpa-sedesol.plataforma_preventiva")
 
 # Variables de ambiente
 load_dotenv(find_dotenv())
+#load_dotenv('../.env')
 
 # AWS
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
 
 class RunPipelines(luigi.WrapperTask):
 
@@ -40,13 +40,11 @@ class RunPipelines(luigi.WrapperTask):
 
     # Pipeline corre mensualmente
     # start_year_month= el pipe de adolfo incluye un start month -> ver rita
-    today = datetime.date.today()
-    year_month = str(today.year) + "-" + str(today.month)
-    year_month_day = year_month + "-" + str(today.day)
+    current_date = luigi.DateParameter(default=datetime.date.today())
 
     def requires(self):
 
-        yield Ingestpipeline(self.year_month)
+        yield Ingestpipeline(self.current_date)
         #yield EtlPipeline(self.year_month)
 
 
@@ -60,34 +58,17 @@ class Ingestpipeline(luigi.WrapperTask):
     # ToDo() Checar la temporalidad de la ingesta: (e.g. si es mensual y toca
     # correr)
 
-    year_month = luigi.Parameter()
+    current_date = luigi.DateParameter()
     conf = configuration.get_config()
 
     # List all pipelines to run
     pipelines = parse_cfg_list(conf.get("Ingestpipeline", "pipelines"))
-    # Get number of cores in which to run pipeline
-    num_cores = 1# multiprocessing.cpu_count()
 
     def requires(self):
 
-        # Note: if there is a 'start_date' parameter, then the info is downloaded monthly and
-        # year_date is assumed to be
-
-        # Obtain the extra parameters
-
-        params = {pipeline: parse_cfg_list(configuration.get_config().get(
-            pipeline, "extra_parameters")) for pipeline in self.pipelines}
-        print(params)
-
-        # Obtain extra parameters: each pipeline has two different parameters:
-        # dates and extra
-        extra = {pipeline: extra_parameters(
-            pipeline, params[pipeline], self.year_month) for pipeline in self.pipelines}
- 
-        # Obtain dates
-        yield Parallel(n_jobs=self.num_cores)(delayed(UpdateDB)(pipeline_task=pipeline, 
-            year_month=str(date), extra=extra_p) for pipeline in self.pipelines for extra_p in 
-            extra[pipeline][1] for date in extra[pipeline][0])
+        # loop through pipeline tasks
+        yield [Concatenation(current_date=self.current_date,
+                             pipeline_task=pipeline) for pipeline in self.pipelines]
 
 
 class EtlPipeline(luigi.WrapperTask):
