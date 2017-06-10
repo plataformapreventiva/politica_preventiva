fa66ad889b25ef46457b625d805172af43d25bf5
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-06-10 01:48:02 +0000

    return True in preprocess scripts to dont break luigi

--- politica_preventiva/pipelines/ingest/preprocessing_scripts/preprocessing_scripts.py
+++ politica_preventiva/pipelines/ingest/preprocessing_scripts/preprocessing_scripts.py
@@ -94,21 +94,25 @@ def indesol_prep(year_month, s3_file, extra_h, out_key):
 def cajeros_banxico_prep(year_month, s3_file, extra_h, out_key):
     bucket = 'dpa-plataforma-preventiva'
     pputils.no_preprocess_method(bucket, 'etl/' + s3_file, out_key)
-
+    return True
 
 def cenapred_prep(year_month, s3_file, extra_h, out_key):
     bucket = 'dpa-plataforma-preventiva'
     pputils.no_preprocess_method(bucket, 'etl/' + s3_file, out_key)
+    return True
 
 def segob_prep(year_month, s3_file, extra_h, out_key):
     bucket = 'dpa-plataforma-preventiva'
     pputils.no_preprocess_method(bucket, 'etl/' + s3_file, out_key)
+    return True
 
 def sagarpa_cierre_prep(year_month, s3_file, extra_h, out_key):
     # TODO: ver si es menor a 2013 (bajado como tabla completa, o mayor, y homologar columnas de ambos casos)
     bucket = 'dpa-plataforma-preventiva'
     pputils.no_preprocess_method(bucket, 'etl/' + s3_file, out_key)
+    return True
 
 def donatarias_sat_prep(year_month, s3_file, extra_h, out_key):
     bucket = 'dpa-plataforma-preventiva'
     pputils.no_preprocess_method(bucket, 'etl/' + s3_file, out_key)
+    return True
