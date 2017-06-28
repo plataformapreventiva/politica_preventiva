2c010381160c429eb0c0f9685cd03d324c65cfab
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-06-27 23:12:29 -0500

    changed transparencia to cuenta publica version

--- politica_preventiva/pipelines/ingest/classic/source/transparencia.sh
+++ politica_preventiva/pipelines/ingest/classic/source/transparencia.sh
@@ -3,14 +3,17 @@
 ###############
 #sudo apt-get install python-dev python-pip python-setuptools build-essential
 #pip install csvkit
+# csvformat -D "|" $1$a > $2
 
-#Todo() Descarga datos del presupuesto-mexico-2008-20164t-2017
+echo "Descarga cuenta publica 2016"
 
-echo "Descarga Presupuesto Desarrollo Social De 2008 a 2017"
+curl 'http://transparenciapresupuestaria.gob.mx/work/models/PTP/DatosAbiertos/BD_Cuenta_Publica/CSV/cuenta_publica_2016_gf_ecd_epe.csv' \
+ -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: es-MX,es;q=0.8,es-419;q=0.6,en;q=0.4' \
+ -H 'Upgrade-Insecure-Requests: 1' \
+ -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' \
+ -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
+ -H 'Referer: http://transparenciapresupuestaria.gob.mx/es/PTP/Datos_Abiertos' \
+ -H 'Cookie: cookiesession1=5F027D86S0WJHUKJYJPJJLZ1CQUB9B58; FGTServer=9DBC1A3C2BDE20F2D0C36FF963399D8AD920C883B29A3E4FC089365787D26EEC6C38848A8522C982F59636F584CA4D27F21B0196E44AED;_ga=GA1.3.1379666131.1497550130; _gid=GA1.3.1973187680.1498619493; __atuvc=17%7C25%2C2%7C26; __atuvs=59531e627721332d001' \
+ -H 'Connection: keep-alive' --compressed | sed s/'\,'/'\|'/g > $2
 
 # Descargar
-
-mkdir -p $1
-a="/temp.csv"
-wget "https://s3.amazonaws.com/datastore.openspending.org/6018ab87076187018fc29c94a68a3cd2/presupuesto-mexico-2008-20164t-2017/data/presupuesto_mexico_2008_20164t_2017.csv" -O $1$a
-csvformat -D "|" $1$a > $2
