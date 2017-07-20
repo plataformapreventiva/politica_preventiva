a13182eccb0328feb6a8c29acc47cde5295e7b8f
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-07-20 22:50:36 +0000

    changed encoding and delimiter to denue table

--- politica_preventiva/pipelines/ingest/classic/source/denue.sh
+++ politica_preventiva/pipelines/ingest/classic/source/denue.sh
@@ -11,13 +11,13 @@ declare -a array
 array[1]="00_62"
 array[2]="00_62_0116"
 array[3]="62_25022015"
-#array[4]="00_62_1016"
-#array[5]="00_61"
-#array[6]="00_61_0116"
-#array[7]="00_61_1016"
-#array[8]="61_25022015"
+array[4]="00_62_1016"
+array[5]="00_61"
+array[6]="00_61_0116"
+array[7]="00_61_1016"
+array[8]="61_25022015"
 
-for i in $(seq 1 3);
+for i in $(seq 1 8);
 do
     echo '############## DOWNLOADING '${array[$i]}' ################'
 
@@ -26,19 +26,19 @@ do
 
     # Borrar zip y otros csv files
     rm $WORK_DIR"/denue_"${array[$i]}"_csv.zip"
-    rm $WORK_DIR/.*denue_diccionario_de_datos.csv
+    find $WORK_DIR/ -regex '.*denue_diccionario.*' -delete
 
     # Mover archivo de interes a carpeta inicial y borrar todo lo demas
     find  $WORK_DIR/denue_${array[$i]}"/" -name "*.csv" -print0 | 
         while IFS= read -r -d $'\0' file; do 
             filename=${array[$i]}_$(basename ${file%.*}).csv; 
-            mv "$file" $WORK_DIR/$filename ; done
+            csvformat -D "|" $file > $WORK_DIR/$filename ; done
+
     rm -r $WORK_DIR"/denue_"${array[$i]}
 done
 
 # Crear array para juntar archivos
-array=( "${array[@]/%/.csv}" )
-array=( "${array[@]/#/$WORK_DIR/}" )
-echo array > ~/temp.txt
+array=$( find  $WORK_DIR/ -regex '.*csv' ) 
+
 # Juntar archivos, añanidendo columna "base" que diga el nombre del archivo origen
-csvstack -n base --filenames ${array[@]} > $2
+csvstack -d "|" -n base -e utf-8 --filenames $array > $2
