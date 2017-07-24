f0637e82f7a761eec888d87227e28132aa192ae5
rsanchezavalos <r.sanchezavalos@gmail.com>
2017-07-24 17:34:08 -0500

    add cuaps dictionary

--- politica_preventiva/pipelines/ingest/classic/source/test_header.py
+++ politica_preventiva/pipelines/ingest/classic/source/test_header.py
@@ -1,8 +1,12 @@
-import yaml
-import os
 import argparse
-import pandas as pd
+import logging
+import os
 import pdb
+import yaml
+
+import pandas as pd
+
+# log = logging.getLogger(__name__)
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -13,54 +17,34 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-def check_header(path, task, new):
-    with open('./pipelines/common/raw_headers.yaml', 'r') as f:
-            doc = yaml.load(f)
+def header_test(path, task, new):
+
+    # Load data from path
     f = open(path)
     data = f.read()
     first_line = data.split('\n', 1)[0]
 
-    if new:
-        doc[task] = first_line
-        with open('./pipelines/common/raw_headers.yaml', 'w') as file:
-            yaml.dump(doc, file, default_flow_style=False)
+    # Load raw schema dic
+    with open('./pipelines/common/raw_schemas.yaml', 'r') as f:
+        doc = yaml.load(f)
 
+    if new:
+        try:
+            doc[task]["RAW"] = first_line
+        except:
+            raise Exception('Problem with the schema definition of {0}- \
+                see common/raw_schemas'.format(task))
     else:
         try:
             old_header = doc[task]
             if old_header != first_line:
-                raise("Error! Header schema has change")
+                raise("Error! Data Source schema has change for task: \
+                 {0}".format(task))
             else:
                 pass
         except:
             doc[task] = first_line
 
-    #with open(path,'r') as f:
-    #    with open(path+".temp",'w') as f1:
-    #        f.next() 
-    #        for line in f:
-    #            f1.write(line)
-    
-    # os.rename(path+".temp", path)
 
-    with open('./pipelines/common/raw_headers.yaml', 'w') as file:
+    with open('./pipelines/common/raw_schemas.yaml', 'w') as file:
         yaml.dump(doc, file, default_flow_style=False)
-
-if __name__=='__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str,
-                    help = 'Path of File')
-
-    parser.add_argument('--task', type=str,
-                    help = 'Name of Task')
-
-    parser.add_argument('--new', type=str2bool, 
-                    nargs='?', const=True, 
-                    help = 'New Pipelines?')
-
-    args = parser.parse_args()
-    path = args.path; task = args.task; new = args.new
-    
-    check_header(path, task, new)
-
