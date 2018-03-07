bdb4541aa2fc75db47cf4f78a7779eac3d1a3755
Andreu Boada de Atela <andreuboada@me.com>
2018-03-06 19:47:23 -0600

    Ingesta de catalogo de programas sociales (#124)

---
+++ politica_preventiva/pipelines/ingest/classic/source/catalogo_padrones.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+####################################
+# Diccionario de programas sociales
+####################################
+
+# Este script ingesta el catalogo de programas carpeta pub-raw, en el S3 del proyecto
+
+# Se toman como parámetros, en ese orden, el data day, el directorio de ingesta y el output path.
+
+data_date=$1
+local_path=$2
+local_ingest_file=$3
+
+echo 'Descarga diccionario'
+
+aws s3 cp s3://pub-raw/diccionarios/catalogo_pub.csv $local_ingest_file
+
+echo 'Written to: '$local_ingest_file
