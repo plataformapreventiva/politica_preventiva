fb5c9207c93c98ccc597bb10bff1e8faf2b9d63e
monzalo14 <monzalo14@gmail.com>
2018-06-21 19:37:13 +0000

    updated tidy tools for orden_gob filters

--- politica_preventiva/pipelines/etl/tools/tidy_tools.R
+++ politica_preventiva/pipelines/etl/tools/tidy_tools.R
@@ -1,8 +1,9 @@
+#!/usr/bin/env Rscript
 # Tidyverse like tools for Postgres
 
 gather_db <- function(df, key, value, not_gathered) {
   key_vals <- setdiff(colnames(df),not_gathered)
-  
+
   get_rows <- function(key_val) {
     df %>%
       select_(.dots = c(not_gathered, key_val)) %>%
@@ -10,22 +11,22 @@ gather_db <- function(df, key, value, not_gathered) {
       rename_(value_ = key_val) %>%
       select_(.dots = c(not_gathered, "key_", "value_"))
   }
-  
+
   df_new <- lapply(key_vals, get_rows)
   Reduce(union_all, df_new) %>%
     rename_(.dots= setNames("key_", key)) %>%
-    rename_(.dots= setNames("value_", value)) 
+    rename_(.dots= setNames("value_", value))
 }
 
 spread_db <- function(df, key, value) {
-  key_vals <- 
+  key_vals <-
     df %>%
     select_(key) %>%
     distinct() %>%
     collect() %>%
     .[[1]] %>%
     sort()
-  
+
   get_rows <- function(key_val) {
     df %>%
       rename_(.dots= setNames(key, "key")) %>%
@@ -33,15 +34,80 @@ spread_db <- function(df, key, value) {
       filter(key == key_val) %>%
       select(-key)
   }
-  
+
   join_vars <- setdiff(colnames(df), c(key, value))
   print(join_vars)
   full_join_alt <- function(x, y) {
     full_join(x, y, by=join_vars)
   }
-  
+
   df_new <- lapply(key_vals, get_rows)
-  Reduce(full_join_alt, df_new) 
+  Reduce(full_join_alt, df_new)
 }
 
+filter_values <- function(x, subset){
+    x_char <- as.character(x)
+    subset_char <- as.character(subset)
+    ifelse(x_char %in% subset_char, x_char, NA)
+}
 
+pull_filler <- function(data, varname){
+  varname_string <- sym(varname)
+  varname_quo <- quo(!! varname_string)
+
+  data %>%
+      pull( !! varname_quo) %>%
+      unique()
+}
+
+tidy_count <- function(data, count_var, plotname, uncounted = c('actualizacion_sedesol', 'data_date'), subset = NULL){
+
+  count_var_name <- sym(count_var)
+  count_var_quo <- quo(!! count_var_name)
+
+  tidy_count_data <-  data %>%
+                        dplyr::group_by_at(vars(one_of(uncounted))) %>%
+                        dplyr::count(!! count_var_quo) %>%
+                        dplyr::mutate(plot = plotname,
+                                      variable = count_var) %>%
+                        dplyr::rename(categoria = !! count_var_quo,
+                                      valor  = n) %>%
+                        dplyr::ungroup() %>%
+                        dplyr::filter(!is.na(categoria)) %>%
+                        dplyr::mutate(categoria = as.character(categoria))
+
+  if(is.null(subset)) return(tidy_count_data)
+
+  subset_values = tibble(categoria = as.character(subset),
+                         actualizacion_sedesol = pull_filler(data, 'actualizacion_sedesol'),
+                         data_date = pull_filler(data, 'data_date'))
+
+  if ('orden_gob' %in% uncounted){
+    subset_values_filtered <- map_df(1:3, function(x) mutate(subset_values, orden_gob = as.character(x)))
+    tidy_count_data <- tidy_count_data %>% mutate_at(vars(orden_gob), as.character)
+  } else {
+      subset_values_filtered <- subset_values
+    }
+
+  tidy_count_data %>%
+      right_join(subset_values_filtered) %>%
+      replace_na(list(plot = plotname,
+                      variable = count_var,
+                      valor = 0))
+}
+
+get_varnames <- function(data, prefix, plot_title, subset_list){
+
+  subset_vector = subset_list[prefix]
+
+  data %>%
+      names() %>%
+      grep(prefix, ., value = T) %>%
+      tibble(varname = .,
+             plotname = plot_title,
+             subset = ifelse(is.null(subset_vector), '', subset_vector))
+}
+
+create_varnames_data <- function(data, varnames, plotnames, subset_list){
+map_df(1:length(varnames), function(x) get_varnames(data, varnames[x], plotnames[x], subset_list))
+}
