96a657a1e1ec2d97b51dcd155ce7ba8f8800c730
monzalo14 <monzalo14@gmail.com>
2018-07-03 23:37:22 +0000

    tidy scripts

---
+++ politica_preventiva/pipelines/etl/tidy/inventario_coneval_estatales.R
@@ -0,0 +1,94 @@
+#!/usr/bin/env Rscript
+library(DBI)
+library(dbplyr)
+library(dplyr)
+library(optparse)
+library(purrr)
+library(rlang)
+library(stringr)
+library(tidyr)
+source("pipelines/etl/tools/tidy_tools.R")
+
+option_list = list(
+  make_option(c("--data_date"), type="character", default="",
+              help="data date", metavar="character"),
+  make_option(c("--database"), type="character", default="",
+              help="database name", metavar="character"),
+  make_option(c("--user"), type="character", default="",
+              help="database user", metavar="character"),
+  make_option(c("--password"), type="character", default="",
+              help="password for datbase user", metavar="character"),
+  make_option(c("--host"), type="character", default="",
+              help="database host name", metavar="character"),
+  make_option(c("--pipeline"), type="character", default="",
+              help="pipeline task", metavar="character")
+);
+
+opt_parser <- OptionParser(option_list=option_list);
+
+opt <- tryCatch(
+        {
+          parse_args(opt_parser);
+        },
+        error=function(cond) {
+            message("Error: Provide database connection arguments appropriately.")
+            message(cond)
+            print_help(opt_parser)
+            return(NA)
+        },
+        warning=function(cond) {
+            message("Warning:")
+            message(cond)
+            return(NULL)
+        },
+        finally={
+            message("Finished attempting to parse arguments.")
+        }
+    )
+
+if(length(opt) > 1){
+
+  if (opt$database=="" | opt$user=="" |
+      opt$password=="" | opt$host=="" ){
+    print_help(opt_parser)
+    stop("Database connection arguments are not supplied.n", call.=FALSE)
+  }else{
+    PGDATABASE <- opt$database
+    POSTGRES_PASSWORD <- opt$password
+    POSTGRES_USER <- opt$user
+    PGHOST <- opt$host
+    PGPORT <- "5432"
+  }
+
+  con <- DBI::dbConnect(RPostgres::Postgres(),
+    host = PGHOST,
+    port = PGPORT,
+    dbname = PGDATABASE,
+    user = POSTGRES_USER,
+    password = POSTGRES_PASSWORD
+  )
+
+  inventario_estatales <- tbl(con,  sql('select * from clean.inventario_coneval_estatales')) %>%
+                         collect()
+  varnames <- 'nb_ent'
+  plotnames <- 's02_estados-estatal'
+  subsets <- inventario_estatales %>% pull(nb_ent) %>% unique()
+
+  inventario_estatales_tidy <- tidy_count(data = inventario_estatales,
+                                          count_var = varnames,
+                                          plotname = plotnames,
+                                          subset = subsets) %>%
+                                mutate(categoria = str_pad(1:32, 2, pad ='0'),
+                                       variable = 'coneval_cve_ent',
+                                       plot_prefix = gsub('(.*)(-.*)', '\\1', plot))
+
+  copy_to(con, inventario_estatales_tidy,
+          dbplyr::in_schema('tidy', 'inventario_coneval_estatales'),
+          temporary = FALSE, overwrite = TRUE)
+
+
+  # disconnect from the database
+  dbDisconnect(con)
+}
+
+
