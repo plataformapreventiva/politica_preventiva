e7a6b165c4eb8489c5b23cbbc05e1f66e90ab7c7
monzalo14 <monzalo14@gmail.com>
2018-07-03 23:09:18 +0000

    Minor changes to source script, re-ingested data with new variables

--- politica_preventiva/pipelines/ingest/classic/source/cuis_historico_domicilio.sh
+++ politica_preventiva/pipelines/ingest/classic/source/cuis_historico_domicilio.sh
@@ -8,7 +8,7 @@
 # Se asume que se tiene acceso a la carpeta sedesol-lab, en el S3 del proyecto
 # Se toman como parámetros, en ese orden, el data date, el directorio de ingesta y el output path
 
-echo "Downloading addresses data. 
+echo "Downloading addresses data.
 Source: CUIS Histórico"
 
 year=$1
@@ -20,7 +20,6 @@ aws s3 cp s3://sedesol-lab/CUIS-HISTORICO/domicilio.zip $local_path/domicilios.z
 echo 'Decompressing zip file'
 
 unzip -c $local_path/domicilios.zip | \
-sed '1,2d' | 
 csvformat -d '^' -D '|' -v > $local_ingest_file
 
 echo 'Written to: '$local_ingest_file
