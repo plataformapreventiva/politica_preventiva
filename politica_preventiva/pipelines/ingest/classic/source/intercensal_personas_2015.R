9ca6b62f9843917faa32cfadc696ed05a3c2de6e
moniduartem <40699542+moniduartem@users.noreply.github.com>
2018-08-30 17:39:15 -0500

    Ingest/intercensal viviendas 2015/mduarte (#176)
    * Added data base and dictionary intercensal_personas_2015
    
    * Added data base and dictionary intercensal_personas_2015
    
    * Added raw headers and config file
    
    * Modified scripts intercensal and database

--- politica_preventiva/pipelines/ingest/classic/source/intercensal_personas_2015.R
+++ politica_preventiva/pipelines/ingest/classic/source/intercensal_personas_2015.R
@@ -16,8 +16,99 @@ personas_02 <- s3read_using(read_csv,
 personas_03 <- s3read_using(read_csv,
                             object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA03.CSV",
                             bucket = "sedesol-lab")
+personas_04 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA04.CSV",
+                            bucket = "sedesol-lab")
+personas_05 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA05.CSV",
+                            bucket = "sedesol-lab")
+personas_06 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA06.CSV",
+                            bucket = "sedesol-lab")
+personas_07 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA07.CSV",
+                            bucket = "sedesol-lab")
+personas_08 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA08.CSV",
+                            bucket = "sedesol-lab")
+personas_09 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA09.CSV",
+                            bucket = "sedesol-lab")
+personas_10 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA10.CSV",
+                            bucket = "sedesol-lab")
+personas_11 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA11.CSV",
+                            bucket = "sedesol-lab")
+personas_12 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA12.CSV",
+                            bucket = "sedesol-lab")
+personas_13 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA13.CSV",
+                            bucket = "sedesol-lab")
+personas_14 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA14.CSV",
+                            bucket = "sedesol-lab")
+personas_15 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA15.CSV",
+                            bucket = "sedesol-lab")
+personas_16 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA16.CSV",
+                            bucket = "sedesol-lab")
+personas_17 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA17.CSV",
+                            bucket = "sedesol-lab")
+personas_18 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA18.CSV",
+                            bucket = "sedesol-lab")
+personas_19 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA19.CSV",
+                            bucket = "sedesol-lab")
+personas_20 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA20.CSV",
+                            bucket = "sedesol-lab")
+personas_21 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA21.CSV",
+                            bucket = "sedesol-lab")
+personas_22 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA22.CSV",
+                            bucket = "sedesol-lab")
+personas_23 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA23.CSV",
+                            bucket = "sedesol-lab")
+personas_24 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA24.CSV",
+                            bucket = "sedesol-lab")
+personas_25 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA25.CSV",
+                            bucket = "sedesol-lab")
+personas_26 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA26.CSV",
+                            bucket = "sedesol-lab")
+personas_27 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA27.CSV",
+                            bucket = "sedesol-lab")
+personas_28 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA28.CSV",
+                            bucket = "sedesol-lab")
+personas_29 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA29.CSV",
+                            bucket = "sedesol-lab")
+personas_30 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA30.CSV",
+                            bucket = "sedesol-lab")
+personas_31 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA31.CSV",
+                            bucket = "sedesol-lab")
+personas_32 <- s3read_using(read_csv,
+                            object = "VULNERABILIDADES-INFORM/Intercensal/Personas/TR_PERSONA32.CSV",
+                            bucket = "sedesol-lab")
 
-personas <- list(personas_01, personas_02, personas_03) %>% 
+personas <- list(personas_01, personas_02, personas_03, personas_04, personas_05, personas_06, personas_07, 
+                 personas_08, personas_09, personas_10, personas_11, personas_12, personas_13, personas_14, 
+                 personas_15, personas_16, personas_17, personas_18, personas_19, personas_20, personas_21, 
+                 personas_22, personas_23, personas_24, personas_25, personas_26, personas_27, personas_28, 
+                 personas_29, personas_30, personas_31, personas_32) %>% 
   rbindlist(., fill = TRUE) %>% as_tibble() 
 
 personas$NOM_MUN <- stri_trans_general(personas$NOM_MUN, "latin-ascii")
--- politica_preventiva/pipelines/ingest/classic/source/intercensal_viviendas_2015.R
+++ politica_preventiva/pipelines/ingest/classic/source/intercensal_viviendas_2015.R
@@ -17,8 +17,99 @@ viviendas_02 <- s3read_using(read_csv,
 viviendas_03 <- s3read_using(read_csv,
                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA03.CSV",
                             bucket = "sedesol-lab")
+viviendas_04 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA04.CSV",
+                             bucket = "sedesol-lab")
+viviendas_05 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA05.CSV",
+                             bucket = "sedesol-lab")
+viviendas_06 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA06.CSV",
+                             bucket = "sedesol-lab")
+viviendas_07 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA07.CSV",
+                             bucket = "sedesol-lab")
+viviendas_08 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA08.CSV",
+                             bucket = "sedesol-lab")
+viviendas_09 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA09.CSV",
+                             bucket = "sedesol-lab")
+viviendas_10 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA10.CSV",
+                             bucket = "sedesol-lab")
+viviendas_11 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA11.CSV",
+                             bucket = "sedesol-lab")
+viviendas_12 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA12.CSV",
+                             bucket = "sedesol-lab")
+viviendas_13 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA13.CSV",
+                             bucket = "sedesol-lab")
+viviendas_14 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA14.CSV",
+                             bucket = "sedesol-lab")
+viviendas_15 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA15.CSV",
+                             bucket = "sedesol-lab")
+viviendas_16 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA16.CSV",
+                             bucket = "sedesol-lab")
+viviendas_17 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA17.CSV",
+                             bucket = "sedesol-lab")
+viviendas_18 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA18.CSV",
+                             bucket = "sedesol-lab")
+viviendas_19 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA19.CSV",
+                             bucket = "sedesol-lab")
+viviendas_20 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA20.CSV",
+                             bucket = "sedesol-lab")
+viviendas_21 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA21.CSV",
+                             bucket = "sedesol-lab")
+viviendas_22 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA22.CSV",
+                             bucket = "sedesol-lab")
+viviendas_23 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA23.CSV",
+                             bucket = "sedesol-lab")
+viviendas_24 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA24.CSV",
+                             bucket = "sedesol-lab")
+viviendas_25 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA25.CSV",
+                             bucket = "sedesol-lab")
+viviendas_26 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA26.CSV",
+                             bucket = "sedesol-lab")
+viviendas_27 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA27.CSV",
+                             bucket = "sedesol-lab")
+viviendas_28 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA28.CSV",
+                             bucket = "sedesol-lab")
+viviendas_29 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA29.CSV",
+                             bucket = "sedesol-lab")
+viviendas_30 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA30.CSV",
+                             bucket = "sedesol-lab")
+viviendas_31 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA31.CSV",
+                             bucket = "sedesol-lab")
+viviendas_32 <- s3read_using(read_csv,
+                             object = "VULNERABILIDADES-INFORM/Intercensal/Viviendas/TR_VIVIENDA32.CSV",
+                             bucket = "sedesol-lab")
 
-viviendas <- list(viviendas_01, viviendas_02, viviendas_03) %>% 
+viviendas <- list(viviendas_01, viviendas_02, viviendas_03, viviendas_04, viviendas_05, viviendas_06, viviendas_07, 
+                 viviendas_08, viviendas_09, viviendas_10, viviendas_11, viviendas_12, viviendas_13, viviendas_14, 
+                 viviendas_15, viviendas_16, viviendas_17, viviendas_18, viviendas_19, viviendas_20, viviendas_21, 
+                 viviendas_22, viviendas_23, viviendas_24, viviendas_25, viviendas_26, viviendas_27, viviendas_28, 
+                 viviendas_29, viviendas_30, viviendas_31, viviendas_32 %>% 
   rbindlist(., fill = TRUE) %>% as_tibble()
 
 viviendas$NOM_MUN <- stri_trans_general(viviendas$NOM_MUN, "latin-ascii")
