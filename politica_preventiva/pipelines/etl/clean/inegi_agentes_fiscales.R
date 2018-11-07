5d1ff86ff2db20a0ee20be35641a4d0ee17066b0
abrownrb <alicia.brownrb@gmail.com>
2018-11-07 14:40:31 -0600

    Features/inegi justicia/abrb (#215)
    * inegi_jueces features
    
    * inegi_agentes_fiscales clean and features

--- politica_preventiva/pipelines/etl/clean/inegi_agentes_fiscales.R
+++ politica_preventiva/pipelines/etl/clean/inegi_agentes_fiscales.R
@@ -4,6 +4,7 @@ library(tidyverse)
 
 float_columns <- c('num_agentes_fiscales')
 text_columns <- c('nom_ent')
+int_columns <- c('anio')
 
 query <- 'SELECT CASE entidad_federativa WHEN \'Ciudad de México\'
                                           THEN \'Distrito Federal\'
@@ -11,12 +12,14 @@ query <- 'SELECT CASE entidad_federativa WHEN \'Ciudad de México\'
                                           THEN \'Querétaro de Arteaga\'
                                           ELSE entidad_federativa
                   END as nom_ent, 
-          num_agentes_fiscales
+          num_agentes_fiscales,
+          anio
           FROM raw.inegi_agentes_fiscales'
 
 make_clean <- function(pipeline_task, con){
   df <- tbl(con, sql(query))
   df %>%
     mutate_at(float_columns, funs(as.numeric(.))) %>%
-    mutate_at(text_columns, funs(as.character(.)))
+    mutate_at(text_columns, funs(as.character(.))) %>%
+    mutate_at(int_columns, funs(as.integer(.)))
 }
---
+++ politica_preventiva/pipelines/features/scripts/inegi_agentes_fiscales.R
@@ -0,0 +1,95 @@
+#!/usr/bin/env Rscript
+library(optparse)
+library(dbplyr)
+library(dplyr)
+library(DBI)
+library(stringr)
+library(yaml)
+
+option_list = list(
+  make_option(c("--data_date"), type="character", default="",
+              help="data date", metavar="character"),
+  make_option(c("--database"), type="character", default="",
+              help="database name", metavar="character"),
+  make_option(c("--user"), type="character", default="",
+              help="database user", metavar="character"),
+  make_option(c("--password"), type="character", default="",
+              help="password for datbase user", metavar="character"),
+  make_option(c("--host"), type="character", default="",
+              help="database host name", metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+
+opt <- tryCatch(
+  {
+    parse_args(opt_parser)
+  },
+  error=function(cond) {
+    message("Error: Provide database connection arguments appropriately.")
+    message(cond)
+    print_help(opt_parser)
+    return(NA)
+  },
+  warning=function(cond) {
+    message("Warning:")
+    message(cond)
+    return(NULL)
+  },
+  finally={
+    message("Finished attempting to parse arguments.")
+  }
+)
+
+if(length(opt) > 1){
+  
+  if (opt$database=="" | opt$user=="" |
+      opt$password=="" | opt$host=="" ){
+    print_help(opt_parser)
+    stop("Database connection arguments are not supplied.n", call.=FALSE)
+  }else{
+    PGDATABASE <- opt$database
+    POSTGRES_PASSWORD <- opt$password
+    POSTGRES_USER <- opt$user
+    PGHOST <- opt$host
+    PGPORT <- "5432"
+  }
+  
+  if(opt$data_date == ""){
+    stop("Did not receive a valid data date, stopping", call.=FALSE)
+  }else{
+    data_date <- opt$data_date
+  }
+  
+  con <- DBI::dbConnect(RPostgres::Postgres(),
+                        host = PGHOST,
+                        port = PGPORT,
+                        dbname = PGDATABASE,
+                        user = POSTGRES_USER,
+                        password = POSTGRES_PASSWORD
+  )
+  
+  source("pipelines/features/tools/features_tools.R")
+  
+  print('Pulling datasets')
+  
+  cves <- tbl(con, dbplyr::in_schema('raw','geom_estados')) %>%
+    select(cve_ent,nom_ent) 
+  
+  agentes_fiscales <- tbl(con, dbplyr::in_schema('clean','inegi_agentes_fiscales')) %>%
+    filter(anio == 2016) %>% 
+    select(nom_ent,num_agentes_fiscales) 
+  
+  agentes_fiscales$nom_ent <- str_replace_all(agentes_fiscales$nom_ent, c("Ciudad de México" = "Distrito Federal"))
+  
+  inegi_agentes_fiscales <- left_join(cves, agentes_fiscales, by = "nom_ent") %>% select(-nom_ent) %>%
+    dplyr::mutate(data_date = data_date,
+                  actualizacion_sedesol = lubridate::today())
+  
+  copy_to(con, inegi_agentes_fiscales,
+          dbplyr::in_schema("features",'inegi_agentes_fiscales'),
+          temporary = FALSE, overwrite = TRUE)
+  dbDisconnect(con)
+  
+  print('Features written to: features.inegi_agentes_fiscales')
+}
