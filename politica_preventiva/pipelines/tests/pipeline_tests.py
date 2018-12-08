415534d8c4a02deffa9378a0e779e9308cb4cc82
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-12-08 04:02:31 +0000

    Fixed bug on data date field

--- politica_preventiva/pipelines/tests/pipeline_tests.py
+++ politica_preventiva/pipelines/tests/pipeline_tests.py
@@ -14,8 +14,8 @@ from io import StringIO
 from luigi import configuration
 import pandas as pd
 from messytables import CSVTableSet, type_guess, \
-		  types_processor, headers_guess, headers_processor, \
-		  offset_processor, any_tableset, ReadError
+          types_processor, headers_guess, headers_processor, \
+          offset_processor, any_tableset, ReadError
 
 
 from politica_preventiva.pipelines.utils.string_cleaner_utils import remove_extra_chars
@@ -27,65 +27,65 @@ logger = logging.getLogger("dpa-sedesol")
 
 
 def dictionary_test(task, dict_path, table_header, dict_header,
-					current_date, data_date, suffix, common_bucket,
-					common_key, allow_joins=False):
-	"""
-	This function updates the task dictionary data with the
-	local dictionary file.	If the task is an ingest task, it will look
-	for the task table header in the raw headers yaml. If it is a
-	features task, it will instead query the database the current features
-	table header.
+                    current_date, data_date, suffix, common_bucket,
+                    common_key, allow_joins=False):
+    """
+    This function updates the task dictionary data with the
+    local dictionary file.  If the task is an ingest task, it will look
+    for the task table header in the raw headers yaml. If it is a
+    features task, it will instead query the database the current features
+    table header.
 
-	# Add a more general way of quoting
-	# TODO() Check if the data has other data date (ingest)
-	# TODO() Add Neo4j legacy task for both ingest and features pipelines
-	# TODO() Rethink dictionary schemas to be more general and standardized
-	if possible
-	"""
-	s3_ = client('s3')
-	s3 = resource('s3')
-	file_name = common_key + task + '_dic.csv'
-	try:
-		try:
-			obj = s3_.get_object(Bucket=common_bucket, Key= file_name)
-			local_dictionary = pd.read_csv(obj['Body'], sep='|', index_col='id')
-		except:
-			local_dictionary = pd.read_csv(dict_path, sep='|', index_col='id')
-		# If the task is a features task, allow to include new variables in
-		# dictionary
-		if allow_joins:
-			header_series = pd.DataFrame(pd.Series(table_header, name='id'))
-			dictionary = header_series.join(local_dictionary, how='left', on='id')
-		else:
-			dictionary = local_dictionary
-			dictionary.reset_index(level=0, inplace=True)
-		assert True not in dictionary['nombre'].isnull().values,\
-				'Your dictionary is not complete'
-		dictionary['actualizacion_sedesol'] = current_date
-		dictionary['data_date'] = data_date + '-' + suffix
-		dictionary = dictionary.where((pd.notnull(dictionary)), None)
-		csv_buffer = StringIO()
-		dictionary.to_csv(csv_buffer,sep='|', encoding='utf-8',
-				quoting=csv.QUOTE_NONE, index=False)
-		s3.Object(common_bucket,
-				file_name).put(Body=csv_buffer.getvalue())
+    # Add a more general way of quoting
+    # TODO() Check if the data has other data date (ingest)
+    # TODO() Add Neo4j legacy task for both ingest and features pipelines
+    # TODO() Rethink dictionary schemas to be more general and standardized
+    if possible
+    """
+    s3_ = client('s3')
+    s3 = resource('s3')
+    file_name = common_key + task + '_dic.csv'
+    try:
+        try:
+            obj = s3_.get_object(Bucket=common_bucket, Key= file_name)
+            local_dictionary = pd.read_csv(obj['Body'], sep='|', index_col='id')
+        except:
+            local_dictionary = pd.read_csv(dict_path, sep='|', index_col='id')
+        # If the task is a features task, allow to include new variables in
+        # dictionary
+        if allow_joins:
+            header_series = pd.DataFrame(pd.Series(table_header, name='id'))
+            dictionary = header_series.join(local_dictionary, how='left', on='id')
+        else:
+            dictionary = local_dictionary
+            dictionary.reset_index(level=0, inplace=True)
+        assert True not in dictionary['nombre'].isnull().values,\
+                'Your dictionary is not complete'
+        dictionary['actualizacion_sedesol'] = current_date
+        dictionary['data_date'] = data_date + '-' + suffix
+        dictionary = dictionary.where((pd.notnull(dictionary)), None)
+        csv_buffer = StringIO()
+        dictionary.to_csv(csv_buffer,sep='|', encoding='utf-8',
+                quoting=csv.QUOTE_NONE, index=False)
+        s3.Object(common_bucket,
+                file_name).put(Body=csv_buffer.getvalue())
 
-	except FileNotFoundError:
-		# Create an empty dataframe with just enough empty cols
-		n = len(dict_header) - 1
-		dictionary = pd.DataFrame([[x] + [''] * n for x in table_header],
-								  columns=dict_header)
-		dictionary['metadata'] = '{}'
-		dictionary['actualizacion_sedesol'] = current_date
-		dictionary['data_date'] = list(data_date + '-' + suffix)
-		dictionary.to_csv(dict_path, index=False,
-						  sep='|', encoding = 'utf-8', quoting=csv.QUOTE_NONE)
-		logger.info('Dictionary of task {task} is not defined,\
-				see {path}'.format(task=features_task, path=dict_path))
-		raise AssertionError('Dictionary of task {0} is not defined,\
-							see {1}'.format(features_task, dict_path))
-	except Exception as e:
-		logger.critical('All fields in name column must be complete')
-		print(e)
-	return dictionary
+    except FileNotFoundError:
+        # Create an empty dataframe with just enough empty cols
+        n = len(dict_header) - 1
+        dictionary = pd.DataFrame([[x] + [''] * n for x in table_header],
+                                  columns=dict_header)
+        dictionary['metadata'] = '{}'
+        dictionary['actualizacion_sedesol'] = current_date
+        dictionary['data_date'] = data_date + '-' + suffix
+        dictionary.to_csv(dict_path, index=False,
+                          sep='|', encoding = 'utf-8', quoting=csv.QUOTE_NONE)
+        logger.info('Dictionary of task {task} is not defined,\
+                see {path}'.format(task=features_task, path=dict_path))
+        raise AssertionError('Dictionary of task {0} is not defined,\
+                            see {1}'.format(features_task, dict_path))
+    except Exception as e:
+        logger.critical('All fields in name column must be complete')
+        print(e)
+    return dictionary
 
